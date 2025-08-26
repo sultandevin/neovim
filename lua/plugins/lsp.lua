3f6dcb4feea955b6425c8efa6608e2da3a911fba
sultandevin <sultan.suyudi@gmail.com>
2025-08-26 20:47:33 +0700

    feat: add luasnip for snippets and autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,5 +22,34 @@ return {
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end
-  }
+  },
+  {
+      'hrsh7th/nvim-cmp',
+      dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'saadparwaiz1/cmp_luasnip', -- Example for snippet integration
+        'L3MON4D3/LuaSnip',
+      },
+      config = function()
+        local cmp = require('cmp')
+        local luasnip = require('luasnip') -- If using snippets
+
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body) -- Example for snippet engine
+            end,
+          },
+          sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+            { name = 'path' },
+            { name = 'luasnip' }, -- If using snippets
+          }),
+          -- Further configuration for keymaps, appearance, etc.
+        })
+      end,
+    }
 }
