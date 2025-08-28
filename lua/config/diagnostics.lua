cc911bcdd4dae935f96abbaafaa06701618045f3
sultandevin <sultan.suyudi@gmail.com>
2025-08-28 09:30:19 +0700

    feat: add inline errors

---
+++ lua/config/diagnostics.lua
@@ -0,0 +1,11 @@
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+  update_in_insert = false,
+	severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true
+  }
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,7 @@ return {
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.diagnostic.enable()
     end
   }
 }
