c5dab10a25297567c3db0c0137349b17ab0617b8
sultandevin <sultan.suyudi@gmail.com>
2025-09-15 22:41:18 +0700

    fix: update config with main commit

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,7 +4,7 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			{
-				"telescope",
+				"ivy",
 			},
 			-- resume = true,
 			oldfiles = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,9 +58,9 @@ return {
 				capabilities = capabilities,
 			})
 
-			lspconfig.go.setup({
-				capabilities = capabilities,
-			})
+			-- lspconfig.go.setup({
+			-- 	capabilities = capabilities,
+			-- })
 
 			lspconfig.html.setup({
 				capabilities = capabilities,
