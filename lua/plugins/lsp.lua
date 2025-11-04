99814c1fdf60713e4766bc4ef1d23e309e3a1faa
sultandevin <sultan.suyudi@gmail.com>
2025-11-05 00:19:34 +0700

    feat: migrate lsp config to new required syntax

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,77 +1,78 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		lazy = false,
-		opts = {},
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-		dependencies = {
-			"mason-org/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+      })
 
       vim.lsp.config("ts_ls", {
         capabilities = capabilities,
-        on_attach = function(_, bufnr)
+        on_attach = function(client, bufnr)
           vim.keymap.set("n", "<leader>oi", function()
             vim.lsp.buf.execute_command({
               command = "_typescript.organizeImports",
               arguments = { vim.api.nvim_buf_get_name(bufnr) },
+              client_id = client.id
             })
           end, { buffer = bufnr, desc = "Organize imports" })
         end,
       })
 
-			vim.lsp.config("tailwindcss", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("tailwindcss", {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.config("jsonls", {
-				capabilities = capabilities,
-			})
+      vim.lsp.config("jsonls", {
+        capabilities = capabilities,
+      })
 
-			vim.lsp.enable({ "ts_ls", "jsonls", "lua_ls" })
+      vim.lsp.enable({ "ts_ls", "jsonls", "lua_ls" })
 
-			--
-			-- lspconfig.svelte.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			--
-			-- lspconfig.tailwindcss.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			--
-			-- lspconfig.html.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			--
-			-- lspconfig.dockerls.setup({
-			-- 	capabilities = capabilities,
-			-- })
-			--
-			-- lspconfig.docker_compose_language_service.setup({
-			-- 	capabilities = capabilities,
-			-- })
+      --
+      -- lspconfig.svelte.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      --
+      -- lspconfig.tailwindcss.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      --
+      -- lspconfig.html.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      --
+      -- lspconfig.dockerls.setup({
+      -- 	capabilities = capabilities,
+      -- })
+      --
+      -- lspconfig.docker_compose_language_service.setup({
+      -- 	capabilities = capabilities,
+      -- })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.diagnostic.enable()
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.diagnostic.enable()
+    end,
+  },
 }
